e093b8b9d821c66bea143ae980b5f4707c49a78a
lwy <337216565@qq.com>
2015-10-19 11:15:53 +0800

    add page and duoshuo

--- article/models.py
+++ article/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -7,6 +8,10 @@ class Article(models.Model):
     category = models.CharField(max_length = 50, blank = True)
     date_time = models.DateTimeField(auto_now_add = True)
     content = models.TextField(blank = True, null = True)
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://127.0.0.1:8000%s" % path
 
     def __unicode__(self):
         return self.title
--- article/views.py
+++ article/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def home(request):
     post_list = Article.objects.all()
+    post_list = get_page(request,post_list,2)
     return render(request,'home.html',{'post_list':post_list})
 
 def detail(request,id):
@@ -14,4 +16,15 @@ def detail(request,id):
     except Article.DoesNotExist:
         raise Http404
     return render(request,'post.html',{'post':post})
+
+def get_page(request, post_list, num=4):
+    paginator = Paginator(post_list, num)
+    try:
+        page = request.GET.get('page')
+        post_list = paginator.page(page)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    return post_list
 # Create your views here.
